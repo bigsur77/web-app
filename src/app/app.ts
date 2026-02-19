ebe8f2be17a28040f5c7a7e662be094224c4ed48
bigsur77 <takutensei@gmail.com>
2026-02-19 13:21:40 +0900

    【2026.2.19】add side menu

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('web-app');
+
+  sidebarOpen = true;
+  navItems = [
+    { path: '/products', label: 'Products' },
+  ];
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  closeSidebarOnMobile() {
+    if (window.innerWidth < 768) {
+      this.sidebarOpen = false;
+    }
+  }
 }
